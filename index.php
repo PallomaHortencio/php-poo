34ae03cf14f27236a21dda0b92f0784433c8a7b3
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-06-08 09:19:20 -0300

    acesso direto aos recursos
    atribuição e leitura de dados em propriedades e métodos publicos.

--- index.php
+++ index.php
@@ -4,17 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 1</title>
+    <title>Exemplo 3</title>
 </head>
 <body>
 
-<h1>PHP  POO - Exemplo 2</h1>
+<h1>PHP  POO - Exemplo 3</h1>
 <hr>
 <h2>Assusntos abordados:</h2>
 <ul>
-    <li>Criação dos objetos</li>
-    <li>Uso do método construtor</li>
-    <li>Uso do <code>$this</code> para acessar as propriedades.</li>
+   <li>Acesso direto á propriedades</li>
+   <li>Atribuição de dados e leitura</li>
 </ul>
 
 <?php
@@ -22,10 +21,36 @@
 require_once "src/Cliente.php";
 
 // Criação dos objetos
-$clienteA = new Cliente('Palloma', 'palloma@gmail.com');
-$clienteB = new Cliente('Igor', 'igor@gmail.com');
+$clienteA = new Cliente;
+$clienteB = new Cliente;
+
+// Atribuindo dados ás propriedades do objeto
+$clienteA->nome = "Palloma";
+$clienteA->email = "palloma@gmail.com";
+$clienteA->telefones = ["11-98445-4648", "11-2247-9645"];
+
+$clienteB->nome = "Igor";
+$clienteB->email = "igor@gmail.com";
+$clienteB->telefones = array("11-97012-1778");
 ?>
-<pre> <?=var_dump($clienteA, $clienteB)?> </pre>
+
+ <!-- <pre> <?=var_dump($clienteA, $clienteB)?> </pre> --> 
+
+
+ <h2>Dados dos objetos (leitura)</h2>
+ <h3> <?=$clienteA->nome?> </h3>
+ <p>E-Mail: <?=$clienteA->email?> </p>
+ <p>Telefones: <?=implode(", ",$clienteA->telefones)?> </p>
+ <p>Senha: <?=$clienteA->senha?> </p>
+
+ <h3> <?=$clienteB->nome?> </h3>
+ <p>E-Mail: <?=$clienteB->email?> </p>
+ <p>Telefones: <?=implode(", ",$clienteB->telefones)?> </p>
+
+
+ <h2>Chamando o metodo exebirDados</h2>
+ <?= $clienteA->exibirDados() ?>
+ <?= $clienteB->exibirDados() ?>
     
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -2,14 +2,17 @@
 class Cliente {
     public string $nome;
     public string $email;
-    public string $senha;
+    public string $senha = "";  /* diexar a string vazia, quando fizer a chamada na pagina index, ela é aceita */
     public array $telefones;
 
-    // Método Construtor
-    public function __construct(string $nome, string $email)
-    {
-        /* $this é uma variável especial, sempre apontando para a referência da Classe atualmente manipulada. */
-        $this->nome = $nome;
-        $this->email = $email;
+    public function exibirDados(){
+        echo "<h3> $this->nome </h3>";
+        echo "<ul>";
+        echo "<li> $this->email </li>";
+        echo "<li> ". implode(', ',$this->telefones)." </li>";
+        echo "</ul>";
     }
+
+
+
 }
